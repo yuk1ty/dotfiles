cd17e6d3fe9a1be0b15b94ec51f3deef9d47c77e
yuk1ty <yuk1ty@users.noreply.github.com>
2022-05-16 23:31:25 +0900

    update around java

--- init.sh
+++ init.sh
@@ -3,3 +3,9 @@ brew bundle
 
 echo "Installing Rust ..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Add Java versions to jenv"
+jenv add /opt/homebrew/Cellar/openjdk/18.0.1/libexec/openjdk.jdk/Contents/Home/
+jenv add /opt/homebrew/Cellar/openjdk@17/17.0.3/libexec/openjdk.jdk/Contents/Home/
+jenv add /opt/homebrew/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home/
+jenv global 18
