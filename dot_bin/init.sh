5451daa246d75837a955496e48544e2831484778
yuk1ty <yuk1ty@users.noreply.github.com>
2022-05-17 11:09:36 +0900

    update init.sh

--- dot_bin/init.sh
+++ dot_bin/init.sh
@@ -1,5 +1,5 @@
 echo "Installing brew stuffs ..."
-if ["$(uname)" != "Darwin"]; then
+if [ "$(uname)" != "Darwin" ]; then
     echo "This machine is not macOS!"
     exit 1
 fi
@@ -9,8 +9,3 @@ brew bundle --file ../Brewfile
 echo "Installing Rust ..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo "Add Java versions to jenv"
-jenv add /opt/homebrew/Cellar/openjdk/18.0.1/libexec/openjdk.jdk/Contents/Home/
-jenv add /opt/homebrew/Cellar/openjdk@17/17.0.3/libexec/openjdk.jdk/Contents/Home/
-jenv add /opt/homebrew/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home/
-jenv global 18
