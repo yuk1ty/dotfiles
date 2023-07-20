c92b80e7c0e76a3bbfab2553d4e223b89bdc1be8
yuk1ty <yuk1ty@users.noreply.github.com>
2023-07-20 16:23:42 +0900

    Update .config/k9s/install.sh

--- dot_config/k9s/install.sh
+++ dot_config/k9s/install.sh
@@ -4,13 +4,9 @@ set -e
 
 # Installation of k9s color scheme for macOS
 
-chezmoi cd
-LOC=$(pwd)
-export=LOC
-
 if [ "$(uname)" != "Darwin" ]; then
     echo "This machine is not macOS!"
     exit 1
 fi
 
-ln -sf "${LOC}/k9s/skin.yml" "${HOME}/Library/Application Support/k9s/skin.yml"
+ln -sf "${HOME}/.config/k9s/skin.yml" "${HOME}/Library/Application Support/k9s/skin.yml"
