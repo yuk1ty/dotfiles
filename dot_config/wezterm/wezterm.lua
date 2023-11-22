52f15ed8c39cdace18cceecb01876cb058475b3f
yuk1ty <yuk1ty@users.noreply.github.com>
2023-11-22 16:33:53 +0900

    Update wezterm setup

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,6 +7,14 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.8
+
+config.colors = {
+  indexed = { [16] = '#1e1e1e' },
+}
+
+config.window_background_opacity = 0.9
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 13.0
 
 return config
