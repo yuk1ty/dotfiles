2d4f3665ce59f1838ad12d9509bac5ece7d358e3
yuk1ty <yuki.multiplus@gmail.com>
2024-10-01 22:21:31 +0900

    I'm aging

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 -- To reset the colour scheme since Catppuccin Mocha sets too bright orange to indexed 16.
 -- This makes me difficult to see operations, for instance, renaming in Neovim.
 config.colors = {
-  indexed = { [16] = '#1e1e1e' },
+	indexed = { [16] = "#1e1e1e" },
 }
 
 config.window_background_opacity = 0.8
@@ -19,11 +19,11 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font_with_fallback({
-  { family = 'JetBrainsMono NF' },
-  { family = 'JetBrainsMono NerdFont' },
-  { family = 'JetBrainsMono' },
-  { family = 'YuGothic' }
+	{ family = "JetBrainsMono NF" },
+	{ family = "JetBrainsMono NerdFont" },
+	{ family = "JetBrainsMono" },
+	{ family = "YuGothic" },
 })
-config.font_size = 12.0
+config.font_size = 13.0
 
 return config
