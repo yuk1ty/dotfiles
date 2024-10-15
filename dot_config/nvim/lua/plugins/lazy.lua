b388390778acfb9bd05d58ffca5002c51b010b3b
yuk1ty <yuki.multiplus@gmail.com>
2024-10-15 15:57:13 +0900

    Add Zen Mode

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -6,4 +6,18 @@ return {
     version = "4.2.0",
     lazy = false,
   },
+  -- Enable Zen Mode
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = {
+        backdrop = 0.9,
+      },
+      plugins = {
+        tmux = { enabled = true },
+        todo = { enabled = true },
+        wezterm = { enabled = true, font = "+2" },
+      },
+    },
+  },
 }
