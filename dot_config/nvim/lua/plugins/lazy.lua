dfa87bed23ed09b277460affa401521238d21ee4
yuk1ty <yuki.multiplus@gmail.com>
2025-02-04 00:01:21 +0900

    Update changes

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -34,7 +34,7 @@ return {
         enabled = true,
         auto_trigger = true,
         hide_during_completion = true,
-        debounce = 75,
+        debounce = 100,
         keymap = {
           accept = "<C-g>l",
           accept_word = false,
