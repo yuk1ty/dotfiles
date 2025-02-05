2e7fa835a9cc9cb703865e1d7583e5b211a71463
yuk1ty <yuki.multiplus@gmail.com>
2025-02-05 13:21:48 +0900

    Disable annoying underline

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -20,7 +20,7 @@ return {
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
       virtual_text = true,
-      underline = true,
+      underline = false,
     },
     -- vim options can be configured here
     options = {
