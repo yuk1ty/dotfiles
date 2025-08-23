6eaae4512df8a1d15f486f2569b7bda6269a7b75
yuk1ty <yuk1ty@users.noreply.github.com>
2025-08-23 23:15:28 +0900

    Turn on all files

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason
 
 ---@type LazySpec
@@ -22,6 +20,9 @@ return {
 
         -- install any other package
         "tree-sitter-cli",
+
+        -- install Kotlin LSP
+        "kotlin-lsp",
       },
     },
   },
--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
