470201c5f848748c2461fe12548dc15af81f0643
yuk1ty <yuk1ty@users.noreply.github.com>
2025-12-19 14:13:34 +0900

    Add gitlinker

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -82,4 +82,14 @@ return {
   { "folke/neoconf.nvim" },
   -- Marp
   { "nwiizo/marp.nvim" },
+  -- GitLink
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
 }
