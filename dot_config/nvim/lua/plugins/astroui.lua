411e2479e19939ee85f7619f1aa677bd1e460998
yuk1ty <yuk1ty@users.noreply.github.com>
2026-04-11 23:48:22 +0900

    Customize Neovim visual selection and Tmux status bar styling

--- dot_config/nvim/lua/plugins/astroui.lua
+++ dot_config/nvim/lua/plugins/astroui.lua
@@ -14,6 +14,8 @@ return {
     highlights = {
       init = { -- this table overrides highlights in all themes
         -- Normal = { bg = "#000000" },
+        Visual = { bg = "#313244" }, -- Catppuccin Mocha Surface0 (subtler selection)
+        StatusLine = { bg = "NONE" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
