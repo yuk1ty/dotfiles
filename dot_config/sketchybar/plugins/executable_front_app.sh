38e3fecd59862d99d00256896f6b629ca211d61e
yuk1ty <yuk1ty@users.noreply.github.com>
2026-04-20 13:14:12 +0900

    Add sketchybar app font and icon mapping support

--- dot_config/sketchybar/plugins/executable_front_app.sh
+++ dot_config/sketchybar/plugins/executable_front_app.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Some events send additional information specific to the event in the $INFO
-# variable. E.g. the front_app_switched event sends the name of the newly
-# focused application in the $INFO variable:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+source "$CONFIG_DIR/plugins/icon_map.sh"
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  __icon_map "$INFO"
+  sketchybar --set "$NAME" \
+             label="$INFO" \
+             icon="$icon_result" \
+             icon.font="sketchybar-app-font:Regular:16.0" \
+             icon.drawing=on
 fi
